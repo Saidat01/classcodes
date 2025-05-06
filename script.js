90d21cc8717fa9f62a454793e1d4f2d3e3fe3fc1
Saidat <baruwande@gmail.com>
2025-05-06 17:08:04 +0100

    codes

---
+++ script.js
@@ -0,0 +1,36 @@
+//const userNumber = parseInt(prompt ("Enter a number"))
+//if (userNumber > 0) {
+//alert (`${userNumber} is a positive number`)
+/// else if(userNumber === 0()) 
+ 
+//  alert (`${userNumber}} is invalid`);
+ 
+//else if( isNaN(userNumber)) 
+     
+// ("this is not a number");
+
+ //else(alert`surd off `); 
+      
+
+// let anyNum = parseInt(prompt ("Enter first number";));
+// let num = parseInt(prompt ("Enter second number";));
+
+
+
+
+
+
+const num = parseInt(prompt ("Enter your result"));
+if (num <= 39) {
+    alert(`${num} is F`)
+}else if (num >39 && num <= 44) {
+    alert(`${num} is E`)
+}else if (num >44 && num <= 49) {
+    alert(`${num} is D`)
+}else if (num >50 && num  <= 59) {
+    alert(`${num} is C`)
+};
+
+
+
+
